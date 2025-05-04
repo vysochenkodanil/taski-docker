65550e7d9ec0a8b244e2cc25b10fc1d8665ca465
VysochenkoDanil <czzzrcoooll@inbox.ru>
2025-05-04 13:43:38 +0300

    Привет код к PEP8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,10 +9,10 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.31.216', 
-    'localhost',       
-    '127.0.0.1',       
-    'czzzrcoooll.zapto.org', 
+    '192.168.31.216',
+    'localhost',
+    '127.0.0.1',
+    'czzzrcoooll.zapto.org',
     '89.169.170.50',
 ]
 
